44130e4e346626803d3161c4aac532bd788fef50
Jason Ouellette <jason@parkstconsulting.com>
2012-07-30 15:38:35 -0700

    appgen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialnamegame::Application.config.secret_token = '1f198da15843521204b74a226b41adca841dbbe41a83d634da71615a3a3942e28cc8a68cc83c176ae398059128411ceb4cd01482cf445388a6542ad587fe257e'
